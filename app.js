1ad7f615b1cc7b65ef552a2e8e45826e9042281f
bstevd <brigham.stevens@gmail.com>
2013-05-20 19:57:09 -0700

    new startup

--- app.js
+++ app.js
@@ -10,10 +10,11 @@ var httpServer = http.createServer(app);
 app.use(express.static(__dirname+"/web"));
 var port = 3000;
 
-
-util.puts('WELCOME to '.blue + 'BOLO '.red + 'v0.1.9.666.x');
-util.puts(' server time: '.blue moment().format('Y-m-d h:i:s').green);
-
+util.puts(' x x x '.orange);
+util.puts('WELCOME to '.blue + 'BOLO '.red + 'v0.1.9.666.x'.orange);
+util.puts(' server time: '.blue + moment().format('Y-m-d h:i:s').green);
+util.puts(' x x x '.orange);
+util.puts('    happy hunting');
 httpServer.listen(port);
 
 var io = sio.listen(httpServer);
