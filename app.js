7f4dfa3028dec5719d051096c379206c8ab26368
bstevd <brigham.stevens@gmail.com>
2013-05-20 19:52:00 -0700

    moment - for manipulating time

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express'),
     sio = require('socket.io'),
     os=require('os'),
+    moment=require('moment'),
     util=require('util'),
     colors=require('colors'),
     http = require('http');
@@ -10,6 +11,8 @@ app.use(express.static(__dirname+"/web"));
 var port = 3000;
 
 
+util.puts('WELCOME to '.blue + 'BOLO '.red + 'v0.1.9.666.x');
+util.puts(' server time: '.blue moment().format('Y-m-d h:i:s').green);
 
 httpServer.listen(port);
 
